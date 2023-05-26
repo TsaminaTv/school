35ca4e17b29550f0710930f6fe9693821cc3faec
TsaminaTv <Tsamina.Tv@gmail.com>
2023-05-26 10:06:27 +0200

    QSDQDS

--- I lost my time and my soul/ex2.py
+++ I lost my time and my soul/ex2.py
@@ -1,4 +1,13 @@
-with open('ex3.txt', 'r', encoding="UTF-8") as f:
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, "ex3.txt")
+
+y =  0
+with open(file_path, 'r', encoding="UTF-8") as f:
     liste = f.readlines()
 for x in liste:
     print(x)
+    y += 1
+    print(f"il y'a {y} lignes")
+print(f"Il y'a {y} lignes au total")
+    
